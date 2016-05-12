93e8e93b4b4c967633bb361d683f42a2a26cde72
teej043 <teej.ebureauet@gmail.com>
2016-05-12 15:01:56 +0800

    changed the sidenav script

--- src/js/side-nav.js
+++ src/js/side-nav.js
@@ -1,3 +1,4 @@
+/*
 $(".btn-menu").click(function(e) {
   $(this).fadeOut(100);
   $(".overlay-menu").fadeIn(100);
@@ -14,6 +15,43 @@ $(".btn-menu").click(function(e) {
 $(".btn-close").click(function(e) {
   closeSideMenu();
 });
+$(".slide-nav div").click(function(e) {
+ $owl.trigger('to.owl.carousel',[$(this).index(),200, true]);
+  closeSideMenu();
+});
+
+$( ".btn-home" ).click( function( e ) {
+  $owl.trigger( 'to.owl.carousel', [0, 200, true] );
+closeSideMenu();
+});
+$( ".btn-contact" ).click( function( e ) {
+  $owl.trigger( 'to.owl.carousel', [11, 200, true] );
+  closeSideMenu();
+});
+
+function closeSideMenu() {
+  $(".btn").animate({
+    left: 195
+  }, {
+    duration: 100,
+    complete: function() {
+      $(".side-menu").animate({ left: -240 }, 200);
+      $(".btn-menu").fadeIn(100);
+      $(".overlay-menu").fadeOut(100);
+    }
+  });
+}
+*/
+var sideNav = $(".side-menu, .overlay-menu, .btn-menu");
+
+$(".btn-menu").on('click',function(e) {
+  sideNav.toggleClass("active");
+});
+
+$(".btn-close").on("click",function(e) {
+  sideNav.toggleClass("active");
+});
+
 $(".slide-nav div").click(function(e) {
  $owl.trigger('to.owl.carousel',[$(this).index(),200, true]);
   closeSideMenu();
